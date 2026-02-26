86ed1b956f06a6196d1b8a05fed6b706608998ea
Glushko Mary <st9213993@stud.duikt.edu.ua>
2026-02-26 13:20:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace pr1_6
+{
+    public delegate bool Validator(string message);
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Validator passcodeValidator = GetValidator(8);
+            Validator logValidator = GetValidator(3);
+
+            Console.Write("Enter passcode:");
+            string passcode = Console.ReadLine();
+            Console.Write("Enter login:");
+            string login = Console.ReadLine();
+
+            Console.WriteLine("Пароль валідний?:" + passcodeValidator(passcode));
+            Console.WriteLine("Логін валідний?:" + logValidator(login));
+        }
+
+        static Validator GetValidator(int minLength)
+        {
+            return message => message.Length >= minLength;
+        }
+    }
+}
