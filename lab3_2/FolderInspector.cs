25b7af4bb2a90796bce040b7474f8e98dbf045af
Glushko Mary <st9213993@stud.duikt.edu.ua>
2026-03-24 17:10:20 +0200

    Add files via upload

---
+++ lab3_2/FolderInspector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr3_2
+{
+    internal class FolderInspector
+    {
+        public void Inspect(string path)
+        {
+            if(!Directory.Exists(path))
+            {
+                Console.WriteLine("Папка не знайдена(((");
+                return;
+            }
+            Console.WriteLine("ПАПКИ:");
+            foreach (string dir in Directory.GetDirectories(path))
+            {
+                Console.WriteLine(dir);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ФАЙЛИ:");
+
+            foreach (string file in Directory.GetFiles(path))
+            {
+                FileInfo info = new FileInfo(file);
+
+                Console.WriteLine("Файл: " + info.Name);
+                Console.WriteLine("Розмір: " + info.Length + " байт");
+                Console.WriteLine("Дата створення: " + info.CreationTime);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ lab3_2/Program2.cs
@@ -0,0 +1,15 @@
+﻿using pr3_2;
+
+namespace pr3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Mary\Desktop\prog\pr3";
+
+            FolderInspector inspector = new FolderInspector();
+            inspector.Inspect(path);
+        }
+    }
+}
