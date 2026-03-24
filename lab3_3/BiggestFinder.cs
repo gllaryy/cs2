652cba876429e3a0c86835fa310f9d6458be48c0
Glushko Mary <st9213993@stud.duikt.edu.ua>
2026-03-24 17:20:17 +0200

    Add files via upload

---
+++ lab3_3/BiggestFinder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr3_3
+{
+    internal class BiggestFinder
+    {
+        public void Find(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("Папка не знайдена!");
+                return;
+            }
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+
+            if (files.Length == 0)
+            {
+                Console.WriteLine("Файлів немає.");
+                return;
+            }
+
+            FileInfo biggest = new FileInfo(files[0]);
+
+            foreach (string file in files)
+            {
+                FileInfo info = new FileInfo(file);
+
+                if (info.Length > biggest.Length)
+                {
+                    biggest = info;
+                }
+            }
+
+            Console.WriteLine("Name: " + biggest.Name);
+            Console.WriteLine("Size: " + biggest.Length + " байт");
+            Console.WriteLine("Path: " + biggest.FullName);
+        }
+    }
+}
---
+++ lab3_3/Program3.cs
@@ -0,0 +1,13 @@
+﻿namespace pr3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Mary\Desktop";
+
+            BiggestFinder finder = new BiggestFinder();
+            finder.Find(path);
+        }
+    }
+}
